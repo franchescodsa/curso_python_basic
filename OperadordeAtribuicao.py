7991a61222357ac06c77994803033277968082a2
franchesco aprigio <franchescodsa@outlook.com>
2024-07-28 20:31:24 -0300

    Atualização de ativdades

---
+++ OperadordeAtribuicao.py
@@ -0,0 +1,19 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
+"""
+    contador = 10
+###
+contador /= 5 
+print(contador)
+"""
+contador = 0
+
+while contador <= 10:
+    print(contador)
+    contador += 1
+    
+
+print('Acabou')
--- While_Break.py
+++ While_Break.py
@@ -8,8 +8,9 @@ Loop infinito -> Quando um código não tem fim
 contador = 0
 
 while contador <= 10:
-    contador = contador + 1
     print(contador)
+    contador = contador + 1
+    
 
 print('Acabou')
 
